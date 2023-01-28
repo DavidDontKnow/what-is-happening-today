4c12372cedea2be1a53c7fe151edd82222371e46
DavidDontKnow <DavidDontKnow02@gmail.com>
2023-01-28 12:27:48 -0800

    added comments/#d classes to descriptions/loadTask

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,17 +2,28 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  displayTime()
+
+  // Get current time
+  displayTime();
+  // Set time to tick each second
   setInterval(displayTime, 1000);
+  // run time tracker to set background indicators
+  timeTracker();
+  // Loads saved tasks from memory
+  loadTask()
+
 
   function displayTime() {
-    var currentTime = $("#currentDay")
+    var currentTime = $("#currentDay");
     var current = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
     currentTime.text(current);
   }
+
+  // save button event listener
   $(".saveBtn").on("click", function () {
     // Get nearby values of the description in JQuery
     var text = $(this).siblings(".description").val();
+    // get time slot id
     var time = $(this).parent().attr("id");
 
     // Save text in local storage
@@ -48,23 +59,21 @@ $(function () {
     })
   }
 
-  timeTracker()
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  function loadTask() {
+    // Sets value of descriptions
+    $(".1").val(localStorage.getItem("hour-6"));
+    $(".2").val(localStorage.getItem("hour-7"));
+    $(".3").val(localStorage.getItem("hour-8"));
+    $(".4").val(localStorage.getItem("hour-9"));
+    $(".5").val(localStorage.getItem("hour-10"));
+    $(".6").val(localStorage.getItem("hour-11"));
+    $(".7").val(localStorage.getItem("hour-12"));
+    $(".8").val(localStorage.getItem("hour-13"));
+    $(".9").val(localStorage.getItem("hour-14"));
+    $(".10").val(localStorage.getItem("hour-15"));
+    $(".11").val(localStorage.getItem("hour-16"));
+    $(".12").val(localStorage.getItem("hour-17"));
+    $(".13").val(localStorage.getItem("hour-18"));
+  }
+
 });
